ef8fe29f6bf4573830d51e58bd19922849b453e2
Lichter Bertalan <lichterbertalan@edu.bme.hu>
2023-12-26 23:51:21 +0100

    minor changes to clean up code

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -3,9 +3,9 @@ use rand::Rng;
 fn main() {
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    let mut has_guessed_correctly = false;
+    let mut num_guesses = 0;
 
-    while !has_guessed_correctly {
+    loop {
         println!("Please choose a number between 1 and 100!");
 
         let mut input = String::new();
@@ -14,16 +14,20 @@ fn main() {
             .read_line(&mut input)
             .expect("Input should be valid!");
 
-        match i32::from_str_radix(&input.trim(), 10) {
+        num_guesses += 1;
+
+        match input.trim().parse::<u8>() {
             Ok(val) => match val.cmp(&secret_number) {
                 std::cmp::Ordering::Less => println!("Too small!"),
                 std::cmp::Ordering::Equal => {
                     println!("You won!");
-                    has_guessed_correctly = true;
+                    break;
                 }
                 std::cmp::Ordering::Greater => println!("Too big!"),
             },
-            Err(_) => eprintln!("Cannot parse int from '{input}'")
+            Err(_) => continue
         };
     }
+
+    println!("Congratulations, you won in {num_guesses} guesses!");
 }
