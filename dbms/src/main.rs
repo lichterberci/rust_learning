f6fc742f8873a8bd4780c2d68eaa09b788ed37a1
lichterberci <lichterberci@gmail.com>
2024-01-28 14:07:37 +0100

    it is soooo ready

--- dbms/src/main.rs
+++ dbms/src/main.rs
@@ -6,7 +6,12 @@ fn main() {
 
         ;--SELECT * FROM table WHERE table.column < 12.3 or table.column2 == false;
 
-        INSERT INTO table (col1, col2, col3) VALUES (3.14, "szia mia", false);-- this here is a comment so it should be ignoerd
+        ;--INSERT INTO table (col1, col2, col3) VALUES (3.14, "szia mia", false);-- this here is a comment so it should be ignoerd
+
+        SELECT tab1.col1, tab2.col2 
+        FROM tab1, tab2 
+        WHERE tab1.col == tab2.col
+        AND tab1.col123 != false OR tab1.col321 < 18.0
 
     "#
     ));
